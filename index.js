470f1330e060636f8634e9f3274a7f2f90a8af20
Andrew Stringfellow <abreezemail@gmail.com>
2024-01-24 16:38:32 -0500

    Added body-parser and json body parsing support

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json())
+
+// Initial request methods
 app.get("/", (req, res) => {
     res.send("Hello World!")
 })
+app.get("/datasend", (req, res) => {
+    res.send("DataSend Response")
+})
+app.get("/datarec", (req, res) => {
+    res.send("DataReceive Response")
+})
+
+
+
+app.post("/datasend/gps", (req, res) => {
+    console.log(req.body)
+    res.send(JSON.stringify(req.body))
+})
+
+
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
